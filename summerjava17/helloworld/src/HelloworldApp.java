f00cf340aa70bd346eb5f4ca10ef19871d7f41c7
clever4 <fatemeh.malekzadeh@yahoo.com>
2017-07-29 17:35:44 +0430

    Helloword

---
+++ summerjava17/helloworld/src/HelloworldApp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+        
+public class HelloworldApp {
+    
+    public static void main(String[] args) {
+      System.out.println ("Helloworld");
+        System.out.println ("welcome to java .");
+         System.out.println("\nprass enter to continue ... ");
+          Scanner input = new Scanner (System.in); 
+           input.nextLine();
+    } 
+}
