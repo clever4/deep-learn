2d65916ba2231e4f70aadac1aa8f8c45c76fae21
clever4 <fatemeh.malekzadeh@yahoo.com>
2017-07-29 21:08:58 +0430

    person

---
+++ summerjava17/person/src/Person.java
@@ -0,0 +1,16 @@
+
+public class Person{
+    private string _name;
+    private int _age;
+    private float _weight;
+    private int _height;   
+
+   public Person()
+{
+
+       _name ="tina";
+       _age ="20";
+       _weight ="45";
+       _height ="170";
+
+       
