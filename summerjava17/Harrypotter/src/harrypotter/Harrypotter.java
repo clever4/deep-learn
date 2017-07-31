78f78879a73f75ff502a46b694d1fc720442e58a
clever4 <fatemeh.malekzadeh@yahoo.com>
2017-07-31 23:06:03 +0430

    Harrypotter

---
+++ summerjava17/Harrypotter/src/harrypotter/Harrypotter.java
@@ -0,0 +1,28 @@
+
+package harrypotter;
+
+
+public class Harrypotter {
+  private String _name;
+  private String _schoolname;
+  private String _housename;
+  private String _blood;
+  private String _birthey;
+  public Harrypotter()
+  {
+     _name = "Harry potter";
+     _schoolname = "hagwards";
+     _housename = "gerefendor";
+     _blood = "pure";
+     birthey ="31.7.1997";
+    public static void main(String[] args) {
+      
+         System.out.format("%name,schoolname,housename,blood,birthey");
+           
+           
+           
+        }
+            
+        }
+    
+}
